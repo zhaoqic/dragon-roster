2a2134dd340c6afef8321ef9a15ca32981265819
zhaoqic <zhaoqic@users.noreply.github.com>
2025-08-01 17:11:28 -0400

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
